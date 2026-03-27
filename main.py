2c8ef738c92615030885b2d578858a1a2f5011a0
Arthur00-crtl <rivabem.campese@gmail.com>
2026-03-27 15:50:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 
 # Configurações das Variáveis de Ambiente
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
-OPENROUTER_API_KEY = os.environ.get('GEMINI_API_KEY')
+OPENROUTER_API_KEY = os.environ.get('OPENROUTER_API_KEY')
 
 bot = telebot.TeleBot(TOKEN)
 
